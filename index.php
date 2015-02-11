a930ad51ca1368a0e3d0ef7ea931b8d20aa0c25e
sundaune <sundaune@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-11 12:16:37 +0000

    Versjon 1.0.5
    git-svn-id: https://plugins.svn.wordpress.org/history-search-by-webloft/trunk@1087094 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -1,1 +1,4 @@
 <?php // Silence is golden
+
+?>
+<img src="http://www.nb.no/services/image/resolver?url_ver=geneza&urn=URN:NBN:no-nb_digibok_2009041700058_C1&maxLevel=5&level=1&col=0&row=0&resX=9000&resY=9000&tileWidth=1024&tileHeight=1024">
